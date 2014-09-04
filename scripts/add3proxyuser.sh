d901c3790145ab364c8f0ec6cb8af7f9cd1c6314
imwifier <imwifier@gmail.com>
2014-09-04 16:23:26 +0800

    Patched source code,now without applying at compile time again

--- scripts/add3proxyuser.sh
+++ scripts/add3proxyuser.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 if [ $4 ]; then  
-	echo $1:`/usr/local/etc/3proxy/bin/mycrypt $$ $2` >> /usr/local/etc/3proxy/passwd
-	echo countin \"`wc -l /usr/local/etc/3proxy/counters|awk '{print $1}'`/$1\" D $3 $1 >> /usr/local/etc/3proxy/counters
-	echo bandlimin $4 $1 >> /usr/local/etc/3proxy/bandlimiters
+	echo $1:`/usr/bin/mycrypt $$ $2` >> /etc/3proxy/passwd
+	echo countin \"`wc -l /etc/3proxy/counters|awk '{print $1}'`/$1\" D $3 $1 >> /etc/3proxy/counters
+	echo bandlimin $4 $1 >> /etc/3proxy/bandlimiters
 else
 	echo usage: $0 username password day_limit bandwidth
 	echo "	"day_limit - traffic limit in MB per day
